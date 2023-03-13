a7a491605b774ee09e2b9518c947ef8c0a48faca
Shivam Patel <110902638+TheShivamPatel@users.noreply.github.com>
2023-03-13 22:28:02 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById('nav-toggle')
-const menue = document.getElementById('nav-right')
-
-toggleBtn.addEventListener('click' , ()=>{
-  menue.classList.toggle('active')
-})
